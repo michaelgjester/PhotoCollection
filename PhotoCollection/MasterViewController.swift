671a4514dba4f4fcb69258f58ee2e5847d4a8af3
michaelgjester <michael.g.jester@gmail.com>
2017-10-04 14:52:19 -0500

    swipe to delete functionality on cells

--- PhotoCollection/MasterViewController.swift
+++ PhotoCollection/MasterViewController.swift
@@ -98,7 +98,8 @@ class MasterViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
+            //objects.remove(at: indexPath.row)
+            self.postArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
